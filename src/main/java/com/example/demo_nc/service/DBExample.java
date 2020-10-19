5d55d11a2513084ba670b8b7d62675c78ca4d0e7
S4AGGY <ranierocj@gmail.com>
2020-10-20 00:01:13 +0300

    https://github.com/S4AGGY/demo_nc.git

---
+++ src/main/java/com/example/demo_nc/service/DBExample.java
@@ -0,0 +1,43 @@
+package com.example.demo_nc.service;
+
+import com.example.demo_nc.CustomErrors;
+import com.example.demo_nc.CustomException;
+import com.example.demo_nc.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+/**
+ * Please, create interface and implement it with class (@Service)
+ */
+public class DBExample implements IDBExample {
+    private List<User> createdUsers = new ArrayList<>();
+    @Override
+    public List<User> getCreatedUsers() {
+        return createdUsers;
+    }
+    @Override
+    public void setCreatedUsers(List<User> createdUsers) {
+        this.createdUsers = createdUsers;
+    }
+    @Override
+    public User createUser(User user) throws CustomException {
+        if (this.createdUsers.stream()
+                .filter(u -> u.getName().equals(user.getName()))
+                .findFirst()
+                .orElse(null) == null) {
+            user.setId(getId());
+            createdUsers.add(user);
+            return user;
+        }
+        else {
+            throw CustomErrors.USER_WITH_SUCH_USERNAME_FOUND.getException();
+        }
+    }
+
+    private Integer getId() {
+        return this.createdUsers.size() + 1;
+    }
+}
